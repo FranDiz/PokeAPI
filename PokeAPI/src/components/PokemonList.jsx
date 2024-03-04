daf202bc7dd74a9c9905d8c915562f656c3892ef
FranDiz <Fran_Diz@outlook.com>
2024-03-04 10:47:25 +0100

    ok

--- PokeAPI/src/components/PokemonList.jsx
+++ PokeAPI/src/components/PokemonList.jsx
@@ -1,35 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState } from "react";
 import PropTypes from 'prop-types';
-import PokemonCard from './PokemonCard.jsx';    
-const PokemonList = (cd) => {
+import PokemonCard from './PokemonCard.jsx';
+import '../assets/styles_components/PokemonList.css';
+const PokemonList = () => {
     const [pokemons, setPokemons] = useState([]);
-    const [pokemonAmount, setPokemonAmount] = useState(0);
-    const getPokemons = async (pokemonAmount) => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${pokemonAmount}`);
+    const [searchTerm, setSearchTerm] = useState('');
+    const getPokemons = async () => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=999999`);
         const data = await res.json();
         setPokemons((prevPokemons) => [...prevPokemons, ...data.results]);
     };
 
-    const morePokemonAmount = () => {
-        setPokemonAmount(pokemonAmount + 50);
+    useEffect(() => {
+        getPokemons();
     }
+    , []);
 
-    useEffect(() => {   
-        getPokemons(pokemonAmount);
-    }, [pokemonAmount]);
-
-
+    
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    }
+    const filteredPokemons = pokemons.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
 
     return (
         <div className="pokemon-list">
+            <div className="search-bar">
+                <input 
+                    type="text" 
+                    placeholder="Buscar Pokémon" 
+                    value={searchTerm} 
+                    onChange={handleSearch} 
+                />
+            </div>
             <div className="pokemon-row">
-                {pokemons.map((pokemon, index) => (
+                {filteredPokemons.map((pokemon, index) => (
                     <div key={index} className="pokemon-card">
                         <PokemonCard url={pokemon.url} />
                     </div>
                 ))}
             </div>
-            <button onClick={morePokemonAmount}>Cargar más</button>
         </div>
     );
 };
