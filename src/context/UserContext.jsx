05aa258d305da4b25ef61ce9e115b5484718a40a
Fran Diz <Fran_Diz@outlook.com>
2024-04-05 12:18:16 +0200

    ok

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,21 +1,11 @@
 import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(()=>{
-        const loggedUser = localStorage.getItem('user');
-        return loggedUser ? JSON.parse(loggedUser) : false;
-    });
-    useEffect(() => {
-        if (user) {
-            localStorage.setItem('user', JSON.stringify(user));
-        } else {
-            localStorage.removeItem('user');
-        }
-    }, [user]);
+    const [user, setUser] = useState(false);
+
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
